96e66566bbda11cb325e29bfb8fc1f90a11e418b
Chung Wei <leongchungwei@hotmail.com>
2024-05-24 15:15:35 +0800

    typo

--- src/server/lib/auth/index.ts
+++ src/server/lib/auth/index.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import { authConfig } from "./config";
-// import { comparePassword } from "./crypto";
+import { comparePassword } from "./crypto";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
